d7ab8f067880d91b92f109d54b2ed36eaf3864e9
nvizcarra <nicolasvizcarra64@gmail.com>
2021-06-15 15:50:33 -0300

    Show Lyrics

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Fragment, useState, useEffect } from 'react';
 import Formulario from './components/Formulario';
 import axios from 'axios';
+import Cancion from './components/Cancion';
 
 function App() {
 
@@ -29,6 +30,23 @@ function App() {
       <Formulario 
         guardarBusquedaLetra={guardarBusquedaLetra}
       />
+
+      <div className="container mt-5">
+
+        <div className="row">
+          <div className="col-md-6">
+            
+          </div>
+
+          <div className="col-md-6">
+            {/* <Letra /> */}
+            <Cancion 
+              letra={letra}
+            />
+          </div>
+        </div>
+
+      </div>
     </Fragment>
   );
 }
